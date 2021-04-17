95977a71848c336b67433f8a5dffabcbb8113b8d
rahulmaheshwari1111 <rahulmaheshwari240@gmail.com>
2021-04-17 22:58:32 +0530

    added dashboard compo

---
+++ src/component/dashboard/Dashboard.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import ProjectList from '../projects/ProjectList'
+import Notification from './Notification'
+
+
+function Dashboard() {
+    return (
+        <div className ="dashboard container">
+            <div className = "row"> 
+                
+                <div className = "col s12 m6"> <ProjectList/></div>
+                <div className = "col s12 m5 offset-m1"> <Notification/>  </div>
+            </div>
+           
+        </div>
+    )
+}
+
+export default Dashboard
