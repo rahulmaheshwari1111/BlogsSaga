da45fa467d7fbf46b1acd856797c79c55c0e7e45
rahulmaheshwari1111 <rahulmaheshwari240@gmail.com>
2021-04-17 22:58:57 +0530

    added notification compo

---
+++ src/component/dashboard/Notification.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Notification() {
+    return (
+        <div>
+            <p> Notifications </p>
+            
+        </div>
+    )
+}
+
+export default Notification
