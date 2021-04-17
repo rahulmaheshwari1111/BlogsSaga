34ab3be817e854b45ef31f2b9c173f3947b6aa6f
rahulmaheshwari1111 <rahulmaheshwari240@gmail.com>
2021-04-17 23:03:53 +0530

    added for summary of blogs

---
+++ src/component/projects/ProjectSummary.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function ProjectSummary() {
+    return (
+        <div>
+            <div className = "card z-depth-0 project-summary">
+                <div className = "card-content grey-text text-darken -3">
+                    
+                    <span className = "card-title"> Project title </span>
+                    <p>Posted by Rahul</p>
+                    <p className = "grey-text"> posted on : 3sept,2020</p>
+                     </div>
+            </div>
+            
+        </div>
+    )
+}
+
+export default ProjectSummary
