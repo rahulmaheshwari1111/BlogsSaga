755390156a19b81faf119bb2241c93710af08f7b
rahulmaheshwari1111 <rahulmaheshwari240@gmail.com>
2021-04-18 23:37:12 +0530

    modified

--- src/component/dashboard/Dashboard.js
+++ src/component/dashboard/Dashboard.js
@@ -1,19 +1,27 @@
 import React from 'react'
 import ProjectList from '../projects/ProjectList'
 import Notification from './Notification'
+import {connect} from 'react-redux'
+ 
 
-
-function Dashboard() {
+function Dashboard(props) {
+    console.log("dash" , props)
+    const project  = props.projects
     return (
         <div className ="dashboard container">
             <div className = "row"> 
                 
-                <div className = "col s12 m6"> <ProjectList/></div>
+                <div className = "col s12 m6"> <ProjectList projects = {project} /></div>
                 <div className = "col s12 m5 offset-m1"> <Notification/>  </div>
             </div>
            
         </div>
     )
 }
+const mapStateToProps = (state)=>{
+   return { 
+       projects: state.project.projects}
 
-export default Dashboard
+}
+
+export default connect(mapStateToProps)(Dashboard)
