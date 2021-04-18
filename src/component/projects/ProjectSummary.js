c2f12d5c9593caed82e784778aa6fbe25247902c
rahulmaheshwari1111 <rahulmaheshwari240@gmail.com>
2021-04-18 23:36:07 +0530

    modified WIP added redux

--- src/component/projects/ProjectSummary.js
+++ src/component/projects/ProjectSummary.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
-function ProjectSummary() {
+function ProjectSummary(props) {
+    console.log("hello",props)
     return (
         <div>
             <div className = "card z-depth-0 project-summary">
                 <div className = "card-content grey-text text-darken -3">
                     
-                    <span className = "card-title"> Project title </span>
+                    <span className = "card-title"> {props.project.title} </span>
                     <p>Posted by Rahul</p>
                     <p className = "grey-text"> posted on : 3sept,2020</p>
                      </div>
