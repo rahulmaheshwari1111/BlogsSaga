da4f80e2020f9e33b4d010c24f8bdadff3cf371c
rahulmaheshwari1111 <rahulmaheshwari240@gmail.com>
2021-04-18 23:38:06 +0530

    added reducers

---
+++ src/store/reducer/projectReducer.js
@@ -0,0 +1,14 @@
+const initState = { 
+    projects:[
+    {id: '1', title: "collect all points", content: "adsdadad"},
+    {id: '2', title: "collect all stars", content: "adsdadadsadgrg"},
+    {id: '3', title: "collect all apples", content: "adsdadadwerwer"}
+]}
+
+const projectReducer = (state = initState,action)=>{
+
+return state
+
+
+}
+export default projectReducer
